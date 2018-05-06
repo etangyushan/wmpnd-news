e0fc63ff0c681465c86b544b64b8663414c589e4
etangyushan <etangyushan@gmail.com>
2018-05-07 00:18:54 +0800

    feat(firstpage)

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,100 +1,86 @@
-const weatherMap = {
-  'sunny': '晴天',
-  'cloudy': '多云',
-  'overcast': '阴',
-  'lightrain': '小雨',
-  'heavyrain': '大雨',
-  'snow': '雪'
+const titleMap = {
+  'gn':'国内',
+  'gj':'国际',
+  'cj': '财经',
+  'yl': '娱乐',
+  'js': '军事',
+  'ty': '体育',
+  'other': '其他',
 }
-
-const weatherColorMap = {
-  'sunny': '#cbeefd',
-  'cloudy': '#deeef6',
-  'overcast': '#c6ced2',
-  'lightrain': '#bdd5e1',
-  'heavyrain': '#c5ccd0',
-  'snow': '#aae1fc'
-}
-
 Page({
   data: {
-    nowTemp: '',
-    nowWeather: '',
-    nowWeatherBackground: "",
-    hourlyWeather: [],
-    todayTemp: "",
-    todayDate: ""
+    subjectList: [],
+    shortNews: [],
+    titleMap: titleMap,
+    type:''
   },
+  //启动时加载
   onLoad(){
-    this.getNow()
-  },
-  onPullDownRefresh(){
-    this.getNow(()=>{
-      wx.stopPullDownRefresh()
+    this.setData({
+      type: 'gn'
     })
+    this.getNews()
   },
-
-  getNow(callback){
+  //获取
+  getNews(callback){
     wx.request({
-      url: 'https://test-miniprogram.com/api/weather/now', //接口地址
+      url: 'https://test-miniprogram.com/api/news/list', //接口地址
       data: {
-        city: '广州市'
+        type: this.data.type
       },
-      success: res=> {
-        //console.log(res.data)
+      success: res => {
+        console.log(res.data)
         let result = res.data.result
-        this.setNow(result)
-        this.setHourlyWeather(result)
-        this.setToday(result)
-        //console.log(this.data.nowWeatherBackground)
+        console.log(result[0].title)
+        this.setNews(result)
       },
+      //结束后执行
       complete: () => {
         callback && callback()
       }
+    })
+    //设置导航栏背景颜色
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: '#cbeefd'
+    })  
+  },
+  //设置简讯
+  setNews(result){
+    console.log('length:' + result.length)
+    let shortNews = []
+    for (let i = 0; i < result.length; i +=1){
+      //console.log(result[i].title)
 
-      
+      shortNews.push({
+        title: result[i].title,
+        date: result[i].date,
+        source: result[i].source,
+        firstImage: result[i].firstImage
+      })
+
+      this.setData({
+        shortNews: shortNews
+      })
+    } 
+  },
+  //点击新闻类型触发
+  onTapNewstype(e) {
+    wx.showToast()
+    //this.setNews()
+    
+    console.log("text:" + e.currentTarget.text)
+  },
+  //点击新闻详细信息
+  onTapNewsdetail() {
+    wx.navigateTo({
+      url: '/pages/detail/detail',
     })
   },
-  setNow(result){
-  let temp = result.now.temp
-  let weather = result.now.weather
-  //console.log(temp, weather)
-  this.setData({
-    nowTemp: temp + '°',
-    nowWeather: weatherMap[weather],
-          
-    nowWeatherBackground: '/images/' + weather + '-bg.png'
-
-  })
-  wx.setNavigationBarColor({
-    frontColor: '#000000',
-    backgroundColor: weatherColorMap[weather],
-  })
-},
-  setHourlyWeather(result){
-  let forecast = result.forecast
-  let hourlyWeather = []
-  let nowHour = new Date().getHours()
-    for (let i = 0; i < 8; i += 1) {
-    //console.log(forecast[i])
-    hourlyWeather.push({
-      time: (i * 3 + nowHour) % 24 + "时",
-      iconPath: '/images/' + forecast[i].weather + '-icon.png',
-        temp: forecast[i].temp + '°'
+  //下拉刷新
+  onPullDownRefresh() {
+    this.getNews(() => {
+      wx.stopPullDownRefresh()
     })
   }
-  hourlyWeather[0].time = '现在'
-  this.setData({
-    hourlyWeather: hourlyWeather
-  })        
-},
-setToday(result){
-  let date = new Date()
-  this.setData({
-      todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
-      todayDate: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} 今天`
-  })
-}
-
-
 })
